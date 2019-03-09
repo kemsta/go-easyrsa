fef6acfdb96caddc32c5edef3401e414c53be5a7
Stanislav Kem <mistiman@gmail.com>
2019-03-09 23:11:31 +0300

    fix comments

--- const.go
+++ const.go
@@ -4,7 +4,7 @@ const (
 	PEMCertificateBlock   string = "CERTIFICATE"     // pem block header for x509.Certificate
 	PEMRSAPrivateKeyBlock        = "RSA PRIVATE KEY" // pem block header for rsa.PrivateKey
 	PEMx509CRLBlock              = "X509 CRL"        // pem block header for CRL
-	CertFileExtension            = ".crt"            // pertificate file extension
-	DefaultKeySizeBytes   int    = 2048              // pefault key size in bytes
-	DefaultExpireYears           = 99                // pefault expire time for certs
+	CertFileExtension            = ".crt"            // certificate file extension
+	DefaultKeySizeBytes   int    = 2048              // default key size in bytes
+	DefaultExpireYears           = 99                // default expire time for certs
 )
